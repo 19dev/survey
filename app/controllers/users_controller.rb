ed1e1a6f20bba96095484fb0d476bbe8843973bb
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2016-01-25 13:38:15 +0200

    bazi ayarlar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,67 @@
 class UsersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 
 
   def index
-    @admins = Admin.all
+    @users = Admin.all
+  end
+  def show
+  	@user = Admin.find(params[:id])
   end
 
+  # GET /polls/new
+  def new
+    @user = Admin.new
+  end
 
+  # GET /polls/1/edit
+  def edit
 
+  end
+
+  # POST /polls
+  # POST /polls.json
+  def create
+    @user = Admin.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+ # PATCH/PUT /polls/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'Poll was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = Admin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:admin).permit(:email,:sign_in_count,:password,:password_confirmation)
+    end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -45,14 +45,26 @@ ActiveRecord::Schema.define(version: 20160125074547) do
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "pass_codes", force: :cascade do |t|
-    t.string   "passcode",              limit: 255
+    t.string   "passcode",               limit: 255
     t.boolean  "pass_code_is_finished"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.integer  "poll_id",               limit: 4
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "poll_id",                limit: 4
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
 
+  add_index "pass_codes", ["email"], name: "index_pass_codes_on_email", unique: true, using: :btree
   add_index "pass_codes", ["poll_id"], name: "index_pass_codes_on_poll_id", using: :btree
+  add_index "pass_codes", ["reset_password_token"], name: "index_pass_codes_on_reset_password_token", unique: true, using: :btree
 
   create_table "polls", force: :cascade do |t|
     t.string   "poll_name",        limit: 255
