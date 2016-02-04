1d3d470767eb267febb2dec137a46cd6e436ec63
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2016-02-04 10:35:44 +0200

    anket sayfası düzenlemesi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,18 @@
 class HomeController < ApplicationController
 	  before_action :set_home, only: [:survey]
 	def  survey
-		@poll = Poll.find(@pid)	
-		@poll_questions = @poll.questions.all.paginate(page: params[:page], per_page: 1)
+	
+			@poll = Poll.find(@pid)	
+			@poll_questions = @poll.questions.all.paginate(page: params[:page], per_page: 1)
+		
 	end
   private
     def set_home
-      @pid=PassCode.find_by_passcode(params["passcod"]).poll_id
+    	if PassCode.find_by_passcode(params["passcod"])
+      		@pid=PassCode.find_by_passcode(params["passcod"]).poll_id
+      	else
+			flash[:notice]="Yanlış giriş kodu girdiniz"
+			redirect_to home_index_path
+		end
     end
 end
