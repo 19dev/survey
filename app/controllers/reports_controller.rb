31e66ada4863c96305cd97c17af5ee67c3871f76
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2016-02-08 15:02:05 +0200

    PassCodeReports Sayfası Düzenlendi

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,19 @@
 class ReportsController < ApplicationController
 	 before_action :set_report, only: [:show, :index, :reportpasscode]
 	def index
-		
+
 	end
+
 	def show
 		@poll=Poll.find(params[:id])
-		@poll_passcodes= @poll.pass_codes.paginate(page: params[:page], per_page: 10)
+		@poll_passcodes = @poll.pass_codes.paginate(page: params[:page], per_page: 10)
 		@questions=@poll.questions
 	end
+
 	def reportpasscode
+		@pass_code = PassCode.find(params[:id])
+		@poll = Poll.find(@pass_code.poll_id)
+		@questions = @poll.questions
 	end
 
 
