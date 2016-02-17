a102540adc1c6bd0a806853196dd9963f1433ab9
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2016-02-17 14:09:53 +0200

    Anket giriş sayfası düzenlemeleri

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -86,6 +86,6 @@ class PollsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_params
-      params.require(:poll).permit(:poll_name, :poll_description, :poll_start_date, :poll_finish_date , questions_attributes: [:id, :question_header,:question_description, :_destroy],poll_images_attributes: [:id, :image, :_destroy])
+      params.require(:poll).permit(:poll_feedback,:poll_name, :poll_description, :poll_start_date, :poll_finish_date , questions_attributes: [:id, :question_header,:question_description, :_destroy],poll_images_attributes: [:id, :image, :_destroy])
     end
 end
