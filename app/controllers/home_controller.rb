002fa3f4e9c80c570caa257255d1af2abd89b1ab
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2016-02-17 10:57:00 +0200

    survey_entrance için pass_code kontrolü.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	  before_action :set_home, only: [:survey, :record]
+	  before_action :set_home, only: [:survey, :survey_entrance, :record]
 	def survey
 
 			if @pass_code.blank?
@@ -28,6 +28,27 @@ class HomeController < ApplicationController
 	end
 
 	def survey_entrance
+			if @pass_code.blank?
+				flash[:notice]="Hatalı giriş kodu kullandınız !"
+				redirect_to home_index_path and return
+
+			else
+
+				if @pass_code.pass_code_is_finished
+					flash[:notice]="Bu giriş kodu daha önce kullanıldı !"
+					redirect_to home_index_path and return
+				end
+				if DateTime.now < @pass_code.poll.poll_start_date
+					flash[:notice]="Bu anket daha başlamadı !"
+					redirect_to home_index_path and return
+				elsif DateTime.now > @pass_code.poll.poll_finish_date
+					flash[:notice]="Bu anketin süresi doldu !"
+					redirect_to home_index_path and return
+				end
+				@poll = Poll.find(@pid)
+				@poll_questions = @poll.questions.all.paginate(page: params[:page], per_page: 1)
+
+			end
 	end
 	
 	def record
@@ -45,12 +66,10 @@ class HomeController < ApplicationController
 
   private
     def set_home
-			if PassCode.where(passcode: params["passcod"]).size != 0
 	    	if PassCode.find_by_passcode(params["passcod"])
 					@pass_code_id = PassCode.find_by_passcode(params["passcod"]).id
 					@pass_code = PassCode.find(@pass_code_id)
-	      	@pid=PassCode.find(@pass_code_id).poll_id
-				end
+	      			@pid=PassCode.find(@pass_code_id).poll_id
 			end
     end
 
