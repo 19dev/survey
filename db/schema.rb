0917578eebeb0f6df51d743cf91acbd4f907f9fd
emrullahsaglam <emrullah.saglam@bil.omu.edu.tr>
2016-05-04 13:52:08 +0300

    Postgresql'e geçiş (Heroku deploy için)

--- db/schema.rb
+++ db/schema.rb
@@ -13,44 +13,47 @@
 
 ActiveRecord::Schema.define(version: 20160218080630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "answers", force: :cascade do |t|
-    t.text     "answer_description", limit: 65535
+    t.text     "answer_description"
     t.boolean  "answer_is_true"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "question_id",        limit: 4
-    t.string   "image",              limit: 255
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "question_id"
+    t.string   "image"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",    limit: 255, null: false
-    t.string   "data_content_type", limit: 255
-    t.integer  "data_file_size",    limit: 4
-    t.integer  "assetable_id",      limit: 4
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
     t.string   "assetable_type",    limit: 30
     t.string   "type",              limit: 30
-    t.integer  "width",             limit: 4
-    t.integer  "height",            limit: 4
+    t.integer  "width"
+    t.integer  "height"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -59,69 +62,69 @@ ActiveRecord::Schema.define(version: 20160218080630) do
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "pass_codes", force: :cascade do |t|
-    t.string   "passcode",              limit: 255
+    t.string   "passcode"
     t.boolean  "pass_code_is_finished"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.integer  "poll_id",               limit: 4
-    t.integer  "person_id",             limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.integer  "poll_id"
+    t.integer  "person_id"
   end
 
   add_index "pass_codes", ["person_id"], name: "index_pass_codes_on_person_id", using: :btree
   add_index "pass_codes", ["poll_id"], name: "index_pass_codes_on_poll_id", using: :btree
 
   create_table "people", force: :cascade do |t|
-    t.string   "person_first_name", limit: 255
-    t.string   "person_last_name",  limit: 255
-    t.text     "person_descr",      limit: 65535
+    t.string   "person_first_name"
+    t.string   "person_last_name"
+    t.text     "person_descr"
     t.date     "person_birthday"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "poll_images", force: :cascade do |t|
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "poll_id",    limit: 4
-    t.string   "image",      limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "poll_id"
+    t.string   "image"
   end
 
   add_index "poll_images", ["poll_id"], name: "index_poll_images_on_poll_id", using: :btree
 
   create_table "polls", force: :cascade do |t|
-    t.string   "poll_name",        limit: 255
-    t.text     "poll_description", limit: 65535
+    t.string   "poll_name"
+    t.text     "poll_description"
     t.date     "poll_start_date"
     t.date     "poll_finish_date"
-    t.text     "poll_feedback",    limit: 65535
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.text     "poll_feedback"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "question_images", force: :cascade do |t|
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "image",       limit: 255
-    t.integer  "question_id", limit: 4
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "image"
+    t.integer  "question_id"
   end
 
   add_index "question_images", ["question_id"], name: "index_question_images_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
-    t.string   "question_header",      limit: 255
-    t.text     "question_description", limit: 65535
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.integer  "poll_id",              limit: 4
+    t.string   "question_header"
+    t.text     "question_description"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "poll_id"
   end
 
   add_index "questions", ["poll_id"], name: "index_questions_on_poll_id", using: :btree
 
   create_table "records", force: :cascade do |t|
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "pass_code_id", limit: 4
-    t.integer  "answer_id",    limit: 4
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "pass_code_id"
+    t.integer  "answer_id"
   end
 
   add_index "records", ["answer_id"], name: "index_records_on_answer_id", using: :btree
